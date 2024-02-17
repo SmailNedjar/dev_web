d530a608263c9e829ea82b53dd29549096cab013
SmailNedjar <smailneo9@gmail.com>
2024-02-17 15:53:44 +0100

    exercice formatio

---
+++ php/25-01-2024/formulaire.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="traitement.php" method="POST">
+        <input type="text" name="champs1" id="champs1">
+        <label for="champs1"></label>
+        <input type="text" name="champs2" id="champs2">
+        <label for="champs2"></label>
+        <input type="submit" value="valider">
+    </form>
+</body>
+</html>
---
+++ php/test/tableau.php
@@ -0,0 +1,75 @@
+<?php
+    $tableau = [1,2,3,4,5,6,7,8,9];
+
+    for ($i = 0; $i < count($tableau); $i++) {
+        echo $tableau[$i];
+    }
+    echo " ";
+
+    foreach ($tableau as $case) {
+        echo $case;
+    }
+
+
+    $tableau = ["prenom"=>"Noel", "nom"=>"bob"];
+        echo " ".$tableau["nom"];
+
+    foreach ($tableau as $key => $val) {
+        echo " ".$key.": ".$val;
+    }
+
+
+    $tableau = [
+        [1,2,3,4],
+        [5,6,7,8],
+        [9,10,11,12],
+        [13,14,15,16]
+    ];
+
+    for ($i = 0; $i < count($tableau); $i++) {
+        for ($j = 0; $j < count($tableau); $j++) {
+            echo " ".$tableau[$i][$j];
+    }
+    }
+
+
+
+    $tableau = [
+        ["nom" =>"doe", "prenom" =>"john"],
+        ["nom" =>"doe", "prenom" =>"john"],
+        ["nom" =>"doe", "prenom" =>"john"]
+    ];
+
+   
+    
+
+
+
+?>
+
+
+<table>
+    <thead>
+        <tr>
+            <th>
+                Nom
+            </th>
+            <th>
+                prenom
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($tableau as $row) {
+        echo "<tr><td>".$row["nom"]."</td>";
+        echo "<td>".$row["prenom"]."</td></tr>";
+    }
+    ?>
+    </tbody>
+</table>
+
+<style>
+    table td {
+        border: 1px solid black;
+    }    
+</style>
