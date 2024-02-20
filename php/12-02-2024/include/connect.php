0e414d10b2efcd54ffe07bd55c8e8470705f958f
Smail_Nedjar <smailneo9@gmail.com>
2024-02-20 12:54:47 +0100

    Delete php directory

--- php/12-02-2024/include/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-try {
-    $db = new PDO("mysql:host=localhost;dbname=bdshop;charset=utf8", "root", "");
-    }catch (PDOException $e) {
-        die("Error: " . $e->getMessage());
-    }
-
-    
--- php/12-02-2024/include/protect.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start();
-if (!isset($_SESSION['user_connexion']) || $_SESSION['user_connexion']!="ok") {
-    header('Location:login.php');
-    exit();
-}
