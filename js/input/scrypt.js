a89465910c369ae44016613668b261e077d381e2
SmailNedjar <smailneo9@gmail.com>
2024-02-23 16:56:23 +0100

    petite correction dossier pro

--- js/input/scrypt.js
+++ js/input/scrypt.js
@@ -8,17 +8,18 @@ const parent = document.querySelector('#parent');
 input.addEventListener('change', (e) => {
     let newdiv = document.createElement('div');
     let span = document.createElement('span');
-    span.innerHTML =" X";
+    let select = document.getElementById('select');
+    let alerte = select.options[select.selectedIndex].value;
+    span.innerHTML = " X " + alerte;
     newdiv.innerHTML = e.target.value;
+
     parent.append(newdiv);
     newdiv.append(span);
+    
     span.addEventListener('click', () => {
     parent.removeChild(newdiv);
     });
     e.target.value="";
 });
-
-
-
 
 
